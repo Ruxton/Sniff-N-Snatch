9c02561baf5724339f56d500516e899248ffe2c1
Ruxton <ruxton@thefrontiergroup.com.au>
2011-04-06 14:38:53 +0800

    Updates to fetch controller

--- app/controllers/fetch_controller.rb
+++ app/controllers/fetch_controller.rb
@@ -1,26 +1,25 @@
 class FetchController < ApplicationController
   
   require 'lib/image_list'
-  require 'lib/fetch_job'
   
   def index
     if !params[:url]
-      render :json => :error, :status => 404
+      render :json => { :status => 'error' }, :status => 404
     else
       url = params[:url]
       
       image_list = ImageList.new(url)
-      images = image_list.get_images
+      images = image_list.get_images      
       images.map! do |href|
         image = Image.where( :address => href ).first
         logger.debug('searched..')
         if image.nil?
           image = Image.create( :address => href )
-          Delayed::Job.enqueue FetchJob.new(image.url_hash)
+#          Delayed::Job.enqueue FetchJob.new(image.url_hash)
         end
-        href = 'http://imgr.local/bucket/image_file/' + image.url_hash.to_s
+        href = image_url(image.url_hash.to_s)
       end
-      render :json => images   
+      render :json => { :status => 'ok', :images => images }
     end
   end
   
